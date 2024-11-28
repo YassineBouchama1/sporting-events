eb507de5a9b4928794fdbf2327ae5e3b491eeebf
siskoweb <sisko4dev@gmail.com>
2024-11-28 11:30:41 +0100

    Fix : solve participants wont delete i use in

--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -3,10 +3,16 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from '../user/schemas/user.schema';
+import { Event, EventSchema } from 'src/event/schemas/event.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
+  imports: [
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: Event.name, schema: EventSchema },
+    ]),
+  ],
   controllers: [UserController],
   providers: [UserService],
 })
-export class UserModule { }
+export class UserModule {}
--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User, UserDocument } from './schemas/user.schema';
@@ -6,10 +6,14 @@ import { Model, Types } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { RoleTypes } from '../common/types/user.enum';
 import * as bcrypt from 'bcrypt';
+import { Event, EventDocument } from 'src/event/schemas/event.schema';
 
 @Injectable()
 export class UserService {
-  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+  constructor(
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+    @InjectModel(Event.name) private eventModel: Model<EventDocument>,
+  ) {}
 
   async findAll(): Promise<User[]> {
     return this.userModel
@@ -25,15 +29,14 @@ export class UserService {
     return user;
   }
 
-  async create( createUserDto: CreateUserDto): Promise<User> {
-
+  async create(createUserDto: CreateUserDto): Promise<User> {
     // generate password from email
     const hashedPassword = await bcrypt.hash(createUserDto.email, 10);
 
     const newParticipant = {
       ...createUserDto,
       password: hashedPassword,
-      role:RoleTypes.Participant
+      role: RoleTypes.Participant,
     };
     const createdParticipant = await this.userModel.create(newParticipant);
     return createdParticipant;
@@ -50,12 +53,39 @@ export class UserService {
   }
 
   async remove(id: string): Promise<User> {
-    const deletedParticipant = await this.userModel
-      .findByIdAndDelete(id)
-      .exec();
-    if (!deletedParticipant) {
-      throw new NotFoundException(`Participant with ID ${id} not found`);
+    try {
+      const participantId = new Types.ObjectId(id);
+
+      //check ifd exist
+      const user = await this.userModel.findById(id);
+      if (!user) {
+        throw new NotFoundException(`Participant with ID ${id} not found`);
+      }
+
+      // remove participant from event usieng pull 
+      await this.eventModel.updateMany(
+        { participants: { $in: [participantId] } },
+        {
+          $pull: {
+            participants: participantId,
+          },
+        },
+      );
+
+      // delete the user
+      const deletedParticipant = await this.userModel
+        .findByIdAndDelete(id)
+        .exec();
+
+      return deletedParticipant;
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw error;
+      }
+      if (error.name === 'CastError') {
+        throw new BadRequestException('Invalid participant ID format');
+      }
+      throw new Error(`Failed to remove participant: ${error.message}`);
     }
-    return deletedParticipant;
   }
 }
