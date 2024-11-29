4695e78a22fc012825c2cd3934f332ff42d9a2ce
siskoweb <sisko4dev@gmail.com>
2024-11-29 14:04:06 +0100

    Fix :solve problem dcker file

--- server/src/main.ts
+++ server/src/main.ts
@@ -42,6 +42,6 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(4000);
+  await app.listen(configService.get('cors.origins') || 4000);
 }
 bootstrap();
